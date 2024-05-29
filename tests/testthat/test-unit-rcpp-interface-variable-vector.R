583dc74096aac4a87975bbfa42b365111e6f13d1
Matthew-Supernaw-NOAA <matthew.supernaw@noaa.gov>
2024-05-29 12:03:51 -0400

    updates to test

--- tests/testthat/test-unit-rcpp-interface-variable-vector.R
+++ tests/testthat/test-unit-rcpp-interface-variable-vector.R
@@ -54,6 +54,86 @@ for(i in 1:v_size){
    expect_equal(v3$at(i)$value, v3_value)
 }
 
+v_acos_test <- acos(v1)
+for(i in 1:v_size){
+   expect_equal(v_acos_test$at(i)$value, acos(v1_value))
+}
+
+
+v_asin_test <- asin(v1)
+for(i in 1:v_size){
+   expect_equal(v_asin_test$at(i)$value, asin(v1_value))
+}
+
+
+v_atan_test <- atan(v1)
+for(i in 1:v_size){
+   expect_equal(v_atan_test$at(i)$value, atan(v1_value))
+}
+
+
+v_cos_test <- cos(v1)
+for(i in 1:v_size){
+   expect_equal(v_cos_test$at(i)$value, cos(v1_value))
+}
+
+
+v_cosh_test <- cosh(v1)
+for(i in 1:v_size){
+   expect_equal(v_cosh_test$at(i)$value, cosh(v1_value))
+}
+
+
+v_sin_test <- sin(v1)
+for(i in 1:v_size){
+   expect_equal(v_sin_test$at(i)$value, sin(v1_value))
+}
+
+
+v_sinh_test <- sinh(v1)
+for(i in 1:v_size){
+   expect_equal(v_sinh_test$at(i)$value, sinh(v1_value))
+}
+
+
+v_tan_test <- tan(v1)
+for(i in 1:v_size){
+   expect_equal(v_tan_test$at(i)$value, tan(v1_value))
+}
+
+
+v_tanh_test <- tanh(v1)
+for(i in 1:v_size){
+   expect_equal(v_tanh_test$at(i)$value, tanh(v1_value))
+}
+
+
+v_exp_test <- exp(v1)
+for(i in 1:v_size){
+   expect_equal(v_exp_test$at(i)$value, exp(v1_value))
+}
+
+
+v_log10_test <- log10(v1)
+for(i in 1:v_size){
+   expect_equal(v_log10_test$at(i)$value, log10(v1_value))
+}
+
+
+v_sqrt_test <- sqrt(v1)
+for(i in 1:v_size){
+   expect_equal(v_sqrt_test$at(i)$value, sqrt(v1_value))
+}
+
+
+v_log_test <- log(v1)
+for(i in 1:v_size){
+   expect_equal(v_log_test$at(i)$value, log(v1_value))
+}
+
+
+
+
 
 #Test that created IDs are unique
 p<-new(ParameterVector, 100)
